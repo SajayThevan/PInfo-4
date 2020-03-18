8847179ed2d454c265c342367320141b42a24d39
Luke Smith <luke.smith@bluewin.ch>
2020-03-18 17:21:11 +0100

    First attempt at the full travis build an deploy

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+# This script is ran on the Virtual Machine at: smithlu7@129.194.69.131
+# It deploys the application 'Kernel'
 
-echo 'I see the script!'
-touch ~/test/test.txt
-echo 'This is working' > touch ~/test/test.txt
+#!/bin/bash
+cd ~/Kernel
+docker login --username=pinfo4 --email=luke.smith@etu.unige.ch
+docker pull pinfo4/api-gateway:latest
+docker pull pinfo4/regulatory-service:latest
+docker pull pinfo4/valuation-service:latest
+docker pull pinfo4/instrument-service:latest
+docker pull pinfo4/counterparty-service:latest
+docker images
+cd  docker-compose
+docker-compose -f docker-compose-microservices.yml up &
+docker-compose -f docker-compose-log.yml up &
+docker-compose -f docker-compose-api-gw.yml up &
