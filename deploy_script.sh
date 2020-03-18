1d8eac8179065effa82c37c66ce27f018be07e17
Luke Smith <luke.smith@bluewin.ch>
2020-03-18 14:29:41 +0100

    Attempting to run deploy script on travis build server rather than ssh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,5 @@
 $!/bin/bash
 
-touch ~/test/test.txt
-echo 'This is working' > touch ~/test/test.txt
+echo 'I see the script!'
+# touch ~/test/test.txt
+# echo 'This is working' > touch ~/test/test.txt
