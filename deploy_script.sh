bdd9c466f8ee4847af8172139b1fb274fa80aef4
Luke Smith <luke.smith@bluewin.ch>
2020-03-21 16:30:59 +0100

    cd $TRAVIS_BUILD_DIR in before_deploy to ensure correct directory

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,6 +5,8 @@
 
 cd ~/Kernel
 # docker login index.docker.io # Credentials setup with pass on the VM
+                               # Note: not needed for now as dockerHub repo is public
+                               # If needed later, need to fix docker-credentials-pass (pass logs out after 2h or so and so docker login can no longer access the credentials)
 docker pull pinfo4/api-gateway:latest
 docker pull pinfo4/regulatory-service:latest
 docker pull pinfo4/valuation-service:latest
