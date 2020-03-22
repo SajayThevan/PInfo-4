8114fe7581234b36e3145bd7116f3e7e42974dc6
Luke Smith <luke.smith@bluewin.ch>
2020-03-22 21:23:01 +0100

    Including pre-commit script in travis.yml so that ssh works

--- pre_deploy_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Prepares the VM for deployment Virtual Machine at: smithlu7@129.194.69.131
-# It deploys the application 'Kernel'
-
-ssh smithlu7@129.194.69.131 'rm -rf ~/Kernel/docker-compose' # Remove the current docker compose
-scp -r docker-compose smithlu7@129.194.69.131:~/Kernel/
