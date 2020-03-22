63b4c2ab91080a4f94aa0d63413046ab17ced4cc
Luke Smith <luke.smith@bluewin.ch>
2020-03-22 18:27:21 +0100

    Adding pre_deploy_script to clean up docker-compose on VM

--- deploy_script.sh
+++ deploy_script.sh
@@ -7,6 +7,12 @@ cd ~/Kernel
 # docker login index.docker.io # Credentials setup with pass on the VM
                                # Note: not needed for now as dockerHub repo is public
                                # If needed later, need to fix docker-credentials-pass (pass logs out after 2h or so and so docker login can no longer access the credentials)
+
+# Clean up docker
+docker container stop $(docker container ls -aq) # Stop all containers
+docker system prune -f -a # remove all stopped containers, dangling and unused images(-a) (& unused networks) , -f to avoid questions
+                          # TODO: '-a' might be too much
+
 docker pull pinfo4/api-gateway:latest
 docker pull pinfo4/regulatory-service:latest
 docker pull pinfo4/valuation-service:latest
