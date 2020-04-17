ee45401d4d46a3eb00055ecea9092ee785eac7cf
Luke.Smith <luke.smith@bluewin.ch>
2020-04-17 18:45:55 +0200

    Updating some shit

--- recipes-service/src/test/java/domain/service/RecipesServiceImplTest.java
+++ recipes-service/src/test/java/domain/service/RecipesServiceImplTest.java
@@ -25,112 +25,25 @@ import eu.drus.jpa.unit.api.JpaUnit;
 
 @ExtendWith(JpaUnit.class)
 @ExtendWith(MockitoExtension.class)
-class InstrumentServiceImplTest {
+class RecipesServiceImplTest {
 
 	@Spy
-	@PersistenceContext(unitName = "InstrumentPUTest")
+	@PersistenceContext(unitName = "RecipesPUTest")
 	EntityManager em;
 
 	@InjectMocks
-	private InstrumentServiceImpl instrumentService;
+	private InstrumentServiceImpl recipesService;
 
-	@Test
-	void testGetAll() {
-		List<Instrument> instruments = instrumentService.getAll();
-		int size = instruments.size();
-		
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		
-		assertEquals(size + 4, instrumentService.getAll().size());
-	}
-
-	@Test
-	void testCount() {
-		List<Instrument> instruments = instrumentService.getAll();
-		int size = instruments.size();
-		
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		instrumentService.create(getRandomInstrument());
-		
-		Long count = instrumentService.count();
-		assertEquals(size + 4, count);
-	}
-	
-	@Test
-	void testUpdate() {
-		instrumentService.create(getRandomInstrument());
-		Instrument instrument = instrumentService.getAll().get(0);
-		assertNotNull(instrument);
-		Long id = instrument.getId();
-		instrument.setOriginalCurrency("XXX");
-		instrumentService.update(instrument);
-		instrument = instrumentService.get(id);
-		assertEquals("XXX", instrument.getOriginalCurrency());
-	}
-	
-	@Test
-	void testUpdateNonExistant() {
-		Instrument i = new Instrument() {
-			@Override
-			public Long getId() {
-				return Long.MAX_VALUE;
-			}
-		};
-		assertThrows(IllegalArgumentException.class, () -> {
-			instrumentService.update(i);
-		});
-	}
-
-	@Test
-	void testGet() {
-		instrumentService.create(getRandomInstrument());
-		Instrument instrument = instrumentService.getAll().get(0);
-		assertNotNull(instrument);
-		Long id = instrument.getId();
-		Instrument getInstrument = instrumentService.get(id);
-		assertEquals(instrument.getOriginalCurrency(), getInstrument.getOriginalCurrency());
-	}
-
-	@Test
-	void testGetNonExistant() {
-		List<Instrument> instruments = instrumentService.getAll();
-		System.out.println("testGetNonExistant:" + instruments.size());
-
-		assertNull(instrumentService.get(Long.MAX_VALUE));
-	}
-
-	@Test
-	void testCreate() {
-		Instrument instrument = getRandomInstrument();
-		instrumentService.create(instrument);
-		assertNotNull(instrument.getId());
-	}
-
-
-	@Test
-	void testCreateDuplicate() {
-		Instrument instrument = getRandomInstrument();
-		instrumentService.create(instrument);
-		assertThrows(IllegalArgumentException.class, () -> {
-			instrumentService.create(instrument);
-		});
-	}
-
-	private Instrument getRandomInstrument() {
-		Bond b = new Bond();
-		b.setBrokerLei(UUID.randomUUID().toString());
-		b.setCounterpartyLei(UUID.randomUUID().toString());
-		b.setAmountInOriginalCurrency(new BigDecimal("0.0"));
-		b.setOriginalCurrency("USD");
-		b.setValueDate(new Date());
-		b.setMaturityDate(new Date());
-		b.setIsin(UUID.randomUUID().toString());
-		b.setQuantity(Long.valueOf(Math.round(Math.random()*1000)));
-		return b;
-	}
+//	@Test
+//	void testGetAll() {
+//		List<Instrument> instruments = instrumentService.getAll();
+//		int size = instruments.size();
+//		
+//		instrumentService.create(getRandomInstrument());
+//		instrumentService.create(getRandomInstrument());
+//		instrumentService.create(getRandomInstrument());
+//		instrumentService.create(getRandomInstrument());
+//		
+//		assertEquals(size + 4, instrumentService.getAll().size());
+//	}
 }
