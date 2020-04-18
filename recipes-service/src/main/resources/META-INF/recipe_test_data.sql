8d8a28088fe8fea19e62e7ae934045b4987f7efc
mathiasTonini <mathias.tonini@gmail.com>
2020-04-18 18:51:13 +0200

    Test addRecipe

--- recipes-service/src/main/resources/META-INF/recipe_test_data.sql
+++ recipes-service/src/main/resources/META-INF/recipe_test_data.sql
@@ -2,19 +2,14 @@ drop table Recipes if exists;
 drop sequence if exists RECIPIES_SEQ;
 create sequence RECIPES_SEQ start with 1 increment by 50;
 create table Recipes (
-        -- instrumentType varchar(31) not null,
-        -- id bigint not null,
+
         recipeID bigint not null,
         name varchar(255) not null,
-        author varchar(255) not null,
+        author int not null,
         data timestamp,
-        category varchar(255),
         difficulty Int,
         time Int,
-        rating Int,
-        -- TO ADD: INGREDIENTS, SETPS AND COMMENT
         primary key (recipeID)
 );
 
---INSERT INTO Instrument (ID, instrumentType, brokerLei, counterpartyLei, dealDate, originalCurrency, valueDate, isin, tracker, quantity, maturityDate, amountInOriginalCurrency, strikeAmount, direction) values ( INSTRUMENT_SEQ.nextval, 'B', '335800DV2WKUAMUTYL21', '254900Z5WUXGPYH1WS92', PARSEDATETIME('10-07-2013','yyyy-dd-mm','en'), 'EUR', PARSEDATETIME('10-07-2013','yyyy-dd-mm','en'), 'BEC0000AIP48', NULL, 4969, PARSEDATETIME('09/22/2020','mm/dd/yyyy','en'), 539235.88, NULL, NULL  );
-INSERT INTO recipes(recipeID, name, author, data, category, difficulty, time, rating) VALUES (RECIPES_SEQ.nextval,'Pâtes au thon', moi, PARSEDATETIME('09/22/2020','mm/dd/yyyy','en'), dinner, 3,2,5);
+INSERT INTO recipes(recipeID, name, author, data, difficulty, time) VALUES (RECIPES_SEQ.nextval,'Pâtes au thon', 1, PARSEDATETIME('09/22/2020','mm/dd/yyyy','en'), 3,2,5);
