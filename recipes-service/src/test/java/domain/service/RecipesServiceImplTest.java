3605f8f5e307c7fd4bc8e35e6fee5061a16d92e0
mathiasTonini <mathias.tonini@gmail.com>
2020-04-19 14:04:09 +0200

    try to find a solution

--- recipes-service/src/test/java/domain/service/RecipesServiceImplTest.java
+++ recipes-service/src/test/java/domain/service/RecipesServiceImplTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.constraints.NotNull;
@@ -23,20 +24,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import domain.model.CategoryEnum;
 import domain.model.Recipe;
-//import eu.drus.jpa.unit.api.JpaUnit;
+import eu.drus.jpa.unit.api.JpaUnit;
 
-//@ExtendWith(JpaUnit.class)
+@ExtendWith(JpaUnit.class)
 @ExtendWith(MockitoExtension.class)
 class RecipesServiceImplTest {
 
 	@Spy
-	@PersistenceContext(unitName = "RecipesPUTest")
+	@PersistenceContext(unitName = "RecipePUTest")
 	EntityManager em;
 
 	@InjectMocks
 	private RecipeServiceImpl recipesService;
 
-	
+	@Test
+	void testDatabase() {
+		em.createNativeQuery("INSERT INTO Recipe (id,authorID,date,difficulty,name,time) VALUES (1,100,'demain',2,'pizza',2)").executeUpdate();
+		List reDB = em.createQuery("SELECT name from Recipe").getResultList();
+		assertEquals(reDB.size(),1);
+	}
 	
 	
 	@Test
@@ -49,16 +55,5 @@ class RecipesServiceImplTest {
 		assertEquals(re.getTime(),1+1);
 	}
 
-//	@Test
-//	void testGetAll() {
-//		List<Instrument> instruments = instrumentService.getAll();
-//		int size = instruments.size();
-//		
-//		instrumentService.create(getRandomInstrument());
-//		instrumentService.create(getRandomInstrument());
-//		instrumentService.create(getRandomInstrument());
-//		instrumentService.create(getRandomInstrument());
-//		
-//		assertEquals(size + 4, instrumentService.getAll().size());
-//	}
+
 }
