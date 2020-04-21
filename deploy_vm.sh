87dbb59d679c954632ac200f44d3f565cce46cae
Luke.Smith <luke.smith@bluewin.ch>
2020-04-21 17:33:56 +0200

    Adding .travis.yml and deploy_vm.sh

---
+++ deploy_vm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is ran on the Virtual Machine at: smithlu7@129.194.69.131
+# It deploys the application 'Kernel' using 'helm install' or 'helm upgrade'
+
+RELEASE=fridge-hub
+helm repo add hung-repo https://pinfo-2020.github.io/PInfo-4/
+helm repo update
+
+if helm list --all-namespaces -a | grep -q $RELEASE
+then
+  echo "$RELEASE exists"
+  helm upgrade $RELEASE hung-repo/microservices
+else
+  echo "$RELEASE doesn't exist"
+  helm install $RELEASE hung-repo/microservices
+fi
