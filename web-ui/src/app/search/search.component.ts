0112b586a363bd17febb30fbb63f16fff3ae0d95
Sajay Thevan <sajay@Sajays-MacBook-Pro.local>
2020-05-15 18:01:38 +0200

    Add Search Bar

--- web-ui/src/app/search/search.component.ts
+++ web-ui/src/app/search/search.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit,} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { IngredientService } from '../services/ingredient/ingredient.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  template: `
+    <div class="example-config">
+      Current value: {{value | json}}
+    </div>
+    <div class="example-wrapper">
+      <p>Favorite sport:</p>
+      <kendo-multiselect [data]="listItems" [(ngModel)]="value"></kendo-multiselect>
+    </div>
+  `
 })
 export class SearchComponent implements OnInit {
 
@@ -20,6 +28,9 @@ export class SearchComponent implements OnInit {
       this.Ingredients = data;
     });
   }
+
+  public listItems: Array<string> = ['Baseball', 'Basketball', 'Cricket', 'Field Hockey', 'Football', 'Table Tennis', 'Tennis', 'Volleyball'];
+  public value: any = [];
     
 
 }
