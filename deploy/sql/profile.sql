83d7a55d8fa87df2c54e8e1aa189f33974beed20
Luke.Smith <luke.smith@bluewin.ch>
2020-05-17 16:12:17 +0200

    Attempting full script after deleting pvc

--- deploy/sql/profile.sql
+++ deploy/sql/profile.sql
@@ -1,10 +1,17 @@
 CREATE USER prf WITH PASSWORD 'prf';
 CREATE ROLE common_role;
-GRANT common_role TO prf, postgres;
+GRANT common_role TO prf;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO common_role;
-SET ROLE prf;
 
+drop table if exists Ingredient;
+drop table if exists RecipeFav;
 drop table if exists Profile cascade;
+drop sequence if exists INGREDIENT_SEQ;
+drop sequence if exists RECIPEFAV_SEQ;
+drop sequence if exists PROFILE_SEQ;
+create sequence INGREDIENT_SEQ start with 1 increment by 50;
+create sequence RECIPEFAV_SEQ start with 1 increment by 50;
+create sequence PROFILE_SEQ start with 1 increment by 50;
 create table Ingredient (
     id bigint not null,
     ingredientId bigint,
@@ -12,5 +19,33 @@ create table Ingredient (
     Profile_id bigint,
     primary key (id)
 );
+create table Profile (
+    id bigint not null,
+    email varchar(255),
+    firstName varchar(255),
+    lastName varchar(255),
+    pseudo varchar(255),
+    score integer not null,
+    primary key (id)
+);
 
-INSERT INTO Profile (ID, email, firstName, lastName, pseudo, score) values ( 1, 'denizsungurtekin@gmail.com', 'deniz', 'gecer', 'malkah', 99);
+create table RecipeFav (
+    id bigint not null,
+    recipeId bigint,
+    Profile_id bigint,
+    primary key (id)
+);
+
+alter table Ingredient
+        add constraint FK9ko8yb2rcb3tvgo925gwwpg0o
+        foreign key (Profile_id)
+        references Profile  ;
+
+alter table RecipeFav
+        add constraint FKhg5gulvbqrg71i0f99s389th5
+        foreign key (Profile_id)
+        references Profile;
+
+INSERT INTO Profile (ID, email, firstName, lastName, pseudo, score) values ( nextval('PROFILE_SEQ'), 'denizsungurtekin@gmail.com', 'deniz', 'gecer', 'malkah', 99);
+INSERT INTO RecipeFav (recipeId, id) values (14, nextval('RECIPEFAV_SEQ'));
+INSERT INTO Ingredient (ingredientId, quantity, id) values (4, 50, nextval('INGREDIENT_SEQ'));
