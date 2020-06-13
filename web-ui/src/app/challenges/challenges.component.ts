97bf567ad5b1775fd88962765748565f5738dde8
Sajay Thevan <sajay@Sajays-MacBook-Pro.local>
2020-06-13 14:48:35 +0200

    WEB UI
    Link Backend-FrontEnd

--- web-ui/src/app/challenges/challenges.component.ts
+++ web-ui/src/app/challenges/challenges.component.ts
@@ -18,41 +18,12 @@ export class ChallengesComponent implements OnInit {
   public quantityForm:FormGroup;
   public quantity:FormControl;
 
-  constructor(private formBuilder:FormBuilder, private ingredientService : IngredientService, private challengeService : ChallengeService) { 
-    this.quantity=new FormControl('',[Validators.required])
-    this.quantityForm=formBuilder.group({
-    quantity:this.quantity
-    })
-  }
-
-  Frigo = []
-  FrigoInter = []
-  Ingredients : Object
-  Ingredient_Name = []
+  constructor(private challengeService:ChallengeService) {}
 
   Challenges$ : Observable<any>;
-  Challenges = [];
+  Challenges : any ;
   
-  //@Input() Frigo: Observable<any>;
   ngOnInit(): void {
-    /*
-    this.ingredientService.getAllIngredientsResearch().subscribe(
-      (data : Response) => {
-        this.Ingredients = data;
-       });
-       // TODO : Appel au backend
-    this.Frigo = [
-      {id:1,quantity : 10},
-      {id:2,quantity : 20},
-      {id:3,quantity : 30},
-      {id:4,quantity : 40},
-      {id:5,quantity : 50},
-      {id:6,quantity : 60}
-    ];
-    this.FrigoInter = this.Frigo;
-    // TODO : ID --> NOM
-    this.Ingredient_Name = ['Lait','Beurre','Choco','Vanille','Pasta','Sucre']
-    */
 
     this.Challenges$ = this.challengeService.getAll()
     this.Challenges$.subscribe(
@@ -62,42 +33,4 @@ export class ChallengesComponent implements OnInit {
       }
     )
   }
-  // ------------- FRIDGE -------------------
-
-  
-
-  selected = [];
-
-  // TODO 
-  // Add modified elements to frige
-  
-  // Take quantity and assign to the correpondant ingredient
-
-  Add(){
-    console.log("Added")
-    console.log(this.quantityForm.get('quantity').value)
-    this.FrigoInter.push({
-      id : this.selected[0].id,
-      quantity : +this.quantityForm.get('quantity').value
-    })
-    this.Ingredient_Name.push(this.selected[0].name)
-    // addIngredientById(profileID,ingredientID, quantity) 
-    console.log(this.Frigo)
-  }
-
-  Remove(id){
-    console.log("Removed")
-    this.FrigoInter.splice(id,1)
-    this.Ingredient_Name.splice(id,1)
-    // removeIngredient(profileID,ingredientID)
-    console.log(this.Frigo)
-  }
-
-  saveFridge(){
-    // this.Frigo --> BACKEND
-    this.Frigo = this.FrigoInter;
-  }
-
-
-  // ------------- END FRIDGE -------------------
 }
