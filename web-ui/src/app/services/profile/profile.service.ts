30c030146759116e074b6647272f6476b6ba1175
mathiasTonini <mathias.tonini@gmail.com>
2020-06-15 12:39:41 +0200

    fix fridge

--- web-ui/src/app/services/profile/profile.service.ts
+++ web-ui/src/app/services/profile/profile.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { stringify } from 'querystring';
+import { retry, catchError } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -39,16 +43,29 @@ export class ProfileService {
       'Content-Type' : 'application/json',
       'Accept': 'application/json'
     });
+
+
     let options = {
       headers: httpHeaders
     };
+    
+    /*console.log(profileID)
+    console.log(ingredientID)
+    console.log(quantity)*/
     var Request_url = environment.profileService.url + "/" + profileID + "/ingredients?ingredient=" + ingredientID + "&quantity=" + quantity;
-    this.http.post(Request_url,null,options);
+    //console.log(Request_url)
+    this.http.post<any>(Request_url,null,options).subscribe({
+      error: error => console.error('There was an error!', error)
+    });
+    return 1
   }
 
   removeIngredient(profileID,ingredientID) {
     var Request_url = environment.profileService.url + "/" + profileID + "/ingredients?ingredient=" + ingredientID;
-    this.http.delete(Request_url);
+    this.http.delete(Request_url).subscribe({
+      error: error => console.error('There was an error!', error)
+    });
+    return 1
   }
 
   // for favourites : 
